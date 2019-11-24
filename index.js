6773071d35d082006a6276a94fa968debe05fcb2
Caleb Kavanaugh <calebck@hotmail.com>
2019-11-24 00:49:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// const names = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(names, occasion) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+   messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+  }
+  return messages
+}
+
+function countDown(i) {
+  while (i > -1) {
+    console.log(i)
+    i--
+  }
+}
